ee5c786cff6d456da8b8f41ec155ea87ae2025ed
Felix Leblanc <felix.leblanc1305@gmail.com>
2015-03-02 07:14:30 -0500

    Credentials file in a gitignore credentials file

--- backend/config/transporter.js
+++ backend/config/transporter.js
@@ -1,11 +1,12 @@
 var nodemailer = require('nodemailer');
+var credentials = require('./credentials');
 
 var transporter = nodemailer.createTransport({
   //Create a credentials file with skel, update README for instructions
   service: 'Gmail',
   auth: {
-    user: 'lanmomo.noreply@gmail.com',
-    pass: 'montmorency2015'
+    user: credentials.mail.username,
+    pass: credentials.mail.password
   }
 });
 
