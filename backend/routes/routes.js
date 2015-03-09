afbff7868cc921dd1f5c714925cbf517d1eaec08
Felix Leblanc <felix.leblanc1305@gmail.com>
2015-03-09 06:30:20 -0400

    Added main controller for non users related routes e.g. /

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,8 +1,8 @@
+var mainController = require('../controllers/main');
 var usersController = require('../controllers/users');
 
 module.exports = function(app) {
-  //TODO in mainController
-  app.get('/', usersController.index);
+  app.get('/', mainController.index);
   app.get('/api/users', usersController.getAll);
   app.post('/api/subscribe', usersController.subscribe);
   app.get('/api/verify/:emailId', usersController.verify);
