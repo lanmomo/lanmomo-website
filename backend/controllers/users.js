3f3ff7707b1361a014d730af907f45baad40a39c
petit-prince <felix.leblanc1305@gmail.com>
2015-03-14 18:16:24 -0400

    logging the info before the response

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -40,8 +40,8 @@ exports.subscribe = function (req, res) {
         config.mail.html = 'Veuillez confirmer votre courriel en cliquant <a href=\"' + url + '\">ici</a>';
         config.transporter.sendMailAsync(config.mail)
         .then(function (info) {
-          res.status(200).json({message:"Veuillez confirmer votre inscription en allant dans votre boîte de réception."})
           console.log(info);
+          res.status(200).json({message:"Veuillez confirmer votre inscription en allant dans votre boîte de réception."})
         })
         .catch(function (err) {
           console.log(err);
