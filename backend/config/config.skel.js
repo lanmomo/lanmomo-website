6c0f7b6affe4082706655d48632227ddf97ee97c
Kiniamaro <vdyotte@gmail.com>
2015-03-16 21:19:14 +0000

    make games grabbed in config instead of DB

--- backend/config/config.skel.js
+++ backend/config/config.skel.js
@@ -11,4 +11,11 @@ module.exports = {
     url: 'mongodb://localhost/dev'
   },
   maximum: 50
+
+  games: [{
+    title: 'Team Fortress 2',
+    description: 'PVP et Mann Versus Machine',
+    imagePath: 'assets/game_banners/tf2.jpg'
+  }]
+
 };
