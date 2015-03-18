27120507c73b2e554690ecc1435f0f904f807640
Patrick Gagnon-Renaud <coding@pgrenaud.com>
2015-03-18 14:28:40 -0400

    Added winston logging library.

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 var mongoose = require('mongoose');
 var express = require('express');
 var bodyParser = require('body-parser');
+var winston = require('winston');
 var morgan = require('morgan');
 var app = express();
 
 var config = require('./backend/config/config');
+
+var wrappedMorgan = morgan(
+  'dev', {
+    stream: {
+      write: function(str) {
+        winston.info(str.slice(0, - 1));
+      }
+    }
+  }
+);
 
 //Database
 mongoose.connect(config.db.url);
@@ -13,11 +24,11 @@ mongoose.connect(config.db.url);
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(morgan('dev'));
+app.use(wrappedMorgan);
 
 //Routing
 require('./backend/routes/routes')(app);
 
 app.listen(config.server.port, function() {
-  console.log('Server listening on port ' + config.server.port);
+  winston.info('Server listening on port %s', config.server.port);
 });
