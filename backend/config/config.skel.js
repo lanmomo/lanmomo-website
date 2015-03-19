a2f0e8e8e85e63523d19ee289c6c07f4e2750f48
Patrick Gagnon-Renaud <coding@pgrenaud.com>
2015-03-19 02:11:56 -0400

    Added games data in config.skel.js file and a banner.

--- backend/config/config.skel.js
+++ backend/config/config.skel.js
@@ -29,10 +29,47 @@ module.exports = {
     pc: 50,
     console: 15
   },
-  games: [{
-    title: 'Team Fortress 2',
-    description: 'PVP et Mann Versus Machine',
-    imagePath: 'assets/game_banners/tf2.jpg'
-  }],
   token: 'token',
+  games: [
+    {
+      title: 'Team Fortress 2',
+      description: 'FPS multijoueur développé par Valve. Les modes de jeux PVP et Mann Versus Machine seront offert.',
+      imagePath: 'assets/game_banners/tf2.jpg'
+    },
+    {
+      title: 'Counter Strike: Source',
+      description: 'FPS en équipe développé par Valve. Les modes poser la bombe et délivrer les otages seront offert.',
+      imagePath: 'assets/game_banners/css.jpg'
+    },
+    {
+      title: 'CS: Global Offensive',
+      description: 'FPS en équipe développé par Valve. Les modes poser la bombe et délivrer les otages seront offert.',
+      imagePath: 'assets/game_banners/cs:go.jpg'
+    },
+    {
+      title: 'Minecraft',
+      description: 'Sandbox multijoueur développé par Mojang. Une course au dragon coopérative en mode survie sera organisée.',
+      imagePath: 'assets/game_banners/minecraft.jpg'
+    },
+    {
+      title: 'Garry\'s Mod',
+      description: 'Sandbox multijoueur développé par Facepunch Studios. Les modes Trouble in Terrorist Town et Prop Hunt sont prévus.',
+      imagePath: 'assets/game_banners/gmod.jpg'
+    },
+    {
+      title: 'TrackMania Nations Forever',
+      description: 'Jeux de course multijoueur développé par Nadeo.',
+      imagePath: 'assets/game_banners/tmnations.jpg'
+    },
+    {
+      title: 'Unreal Tournament 2004',
+      description: 'FPS futuriste développé par Epic Games et Digital Extremes.',
+      imagePath: 'assets/game_banners/ut2004.jpg'
+    },
+    {
+      title: 'League of Legends',
+      description: 'Arène de bataille multijoueur développée par Riot Games.',
+      imagePath: 'assets/game_banners/lol.jpg'
+    }
+  ]
 };
