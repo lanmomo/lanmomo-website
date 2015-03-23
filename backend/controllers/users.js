78b941ffcd048c465bd8bf4c9a73ba7ec5311a5e
Justin Duplessis <jduplessis@iweb.com>
2015-03-23 10:50:22 -0400

    Fix missings spaces
    Félix n'avait pas fait de tests :p

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -99,9 +99,9 @@ var sendMail = function(req, res, emailVerification) {
 };
 
 var createEmail = function(url, emailVerification) {
-  var template = 'Bonjour %s, <br><br>Veuillez confirmer votre courriel en' +
-    'cliquant sur le lien suivant:<br><a href="%s">%s</a><br><br>Merci' +
-    'et à bientôt!<br><br><small>Ceci est un courriel envoyé' +
+  var template = 'Bonjour %s, <br><br>Veuillez confirmer votre courriel en ' +
+    'cliquant sur le lien suivant:<br><a href="%s">%s</a><br><br>Merci ' +
+    'et à bientôt!<br><br><small>Ceci est un courriel envoyé ' +
     'automatiquement. Veuillez ne pas y répondre.</small>';
   return new P(function(resolve, reject) {
     User.findOne({_id: emailVerification.userId}).select('firstname').exec()
