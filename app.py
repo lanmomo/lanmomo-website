ece3fd4d3564c32496674911707ad175295ffce9
Justin Duplessis <drfoliberg@gmail.com>
2015-09-21 11:54:07 -0400

    PEP8 + shebang

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 import sys
 import json
@@ -7,13 +8,16 @@ from database import db_session, init_db, init_engine
 
 app = Flask(__name__)
 
+
 @app.route('/api/games', methods=['GET'])
 def get_games():
     return jsonify(games), 200
 
+
 @app.route('/api/servers', methods=['GET'])
 def get_servers():
     return jsonify({'error': 'Not implemented'}), 500
+
 
 @app.route('/api/servers', methods=['POST'])
 def update_server():
@@ -24,9 +28,11 @@ def update_server():
 def index():
     return send_from_directory('public', 'index.html')
 
+
 @app.route('/<path:path>')
 def static_files(path):
     return send_from_directory('public', path)
+
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
@@ -40,7 +46,6 @@ if __name__ == '__main__':
         conf = sys.argv[1]
         print('Loading additionnal config ' + conf)
         app.config.from_pyfile('config/' + conf + '_config.py')
-
 
     with open('config/games.json') as data_file:
         games = json.load(data_file)
--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import mapper
 
 from database import metadata, db_session
 
+
 class User():
     query = db_session.query_property()
 
@@ -15,10 +16,12 @@ class User():
     def __repr__(self):
         return '<User %r>' % (self.username)
 
+
 class Ticket():
     query = db_session.query_property()
 
-    def __init__(self, type_id, owner_id, paid=False, reserved_until=None, reserved_at=None):
+    def __init__(self, type_id, owner_id, paid=False, reserved_until=None,
+                 reserved_at=None):
         self.type_id = type_id
         self.owner_id = owner_id
         self.paid = paid
@@ -29,21 +32,25 @@ class Ticket():
         return '<Ticket %r>' % (self.id)
 
 users = Table('users', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('username', String(1000), nullable=False),
-    Column('email', String(1000), nullable=False),
-    Column('password', Binary(64), nullable=False),
-    Column('salt', String(42), nullable=False)
-)
+              Column('id', Integer, primary_key=True),
+              Column('username', String(1000), nullable=False),
+              Column('email', String(1000), nullable=False),
+              Column('password', Binary(64), nullable=False),
+              Column('salt', String(42), nullable=False)
+              )
 
 tickets = Table('tickets', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('type_id', Integer, nullable=False), # pc or console
-    Column('owner_id', Interger, ForeignKey("user.id"), nullable=False), # avoid n-n for now...
-    Column('paid', Boolean, default=False, nullable=False), # Look for related payment and remove this field ?
-    Column('reserved_until', Date, nullable=False),
-    Column('reserved_at', Date, default=_get_date, nullable=False)
-)
+                Column('id', Integer, primary_key=True),
+                # pc or console
+                Column('type_id', Integer, nullable=False),
+                # avoid n-n for now...
+                Column('owner_id', Interger, ForeignKey("user.id"),
+                       nullable=False),
+                # Look for related payment and remove this field ?
+                Column('paid', Boolean, default=False, nullable=False),
+                Column('reserved_until', Date, nullable=False),
+                Column('reserved_at', Date, default=_get_date, nullable=False)
+                )
 
 mapper(User, users)
 mapper(Ticket, tickets)
