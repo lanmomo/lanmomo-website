26286609120198a4426d829b00c09f9c919b48a7
Justin Duplessis <drfoliberg@gmail.com>
2015-09-21 01:41:54 -0400

    wip models

--- models.py
+++ models.py
@@ -6,13 +6,23 @@ from database import metadata, db_session
 class User():
     query = db_session.query_property()
 
-    def __init__(self, username, password, salt):
+    def __init__(self, username, email, password, salt):
         self.username = username
+        self.email = email
         self.password = password
         self.salt = salt
 
     def __repr__(self):
         return '<User %r>' % (self.username)
+
+class Ticket():
+    query = db_session.query_property()
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return '<Ticket %r>' % (self.id)
 
 users = Table('users', metadata,
     Column('id', Integer, primary_key=True),
@@ -22,4 +32,12 @@ users = Table('users', metadata,
     Column('salt', String(42))
 )
 
+tickets = Table('tickets', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('type_id', Integer), # pc or console
+    Column('reserved_until', Date),
+
+)
+
 mapper(User, users)
+mapper(Ticket, tickets)
