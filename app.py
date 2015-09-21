972fc4a604f70093734ce1e6d4b66f5e55383732
Justin Duplessis <drfoliberg@gmail.com>
2015-09-21 02:13:43 -0400

    Reimplement the api games GET

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
 import sys
+import json
 
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
 from database import db_session, init_db, init_engine
 
 app = Flask(__name__)
 
 @app.route('/api/games', methods=['GET'])
 def get_games():
-    return jsonify({'error': 'Not implemented'}), 500
+    return jsonify(games), 200
 
 @app.route('/api/servers', methods=['GET'])
 def get_servers():
@@ -35,11 +36,14 @@ def shutdown_session(exception=None):
 if __name__ == '__main__':
     app.config.from_pyfile('config/default_config.py')
 
-    # TODO load games from 'games.json'
     if len(sys.argv) == 2:
         conf = sys.argv[1]
         print('Loading additionnal config ' + conf)
         app.config.from_pyfile('config/' + conf + '_config.py')
+
+
+    with open('config/games.json') as data_file:
+        games = json.load(data_file)
 
     init_engine(app.config['DATABASE_URI'])
     init_db()
