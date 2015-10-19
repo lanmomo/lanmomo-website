75983f5a2ead622317b405f2f87d836294c7522c
kiniamaro <vdyotte@gmail.com>
2015-10-18 21:54:42 -0400

    add team_size check team rename and tournament in backend

--- app.py
+++ app.py
@@ -112,6 +112,17 @@ def user_in_team(game, user_id):
     return False
 
 
+def team_is_full(id):
+    team = Team.query.filter(Team.id == id).first()
+    if not team:
+        return false
+    members = TeamUser.query.filter(TeamUser.team_id == id).count()
+    for tournament in tournaments['tournaments']:
+        if tournament['game'] == team.game:
+            team_size = tournament['team_size']
+    return members >= team_size - 1  # minus the captain
+
+
 def send_email(to_email, to_name, subject, message, attachements=None):
     mail.send_email(to_email, to_name, subject, message,
                     app.config['MAILGUN_USER'], app.config['MAILGUN_KEY'],
@@ -142,11 +153,14 @@ def join_team():
     req = request.get_json()
     team_user = TeamUser(req['team_id'], user_id)
     if user_in_team(req['game'], user_id):
-        return jsonify({'message': "Vous êtes déja dans une équipe "}), 400
+        return jsonify({'message': "Vous êtes déja dans une équipe."}), 400
+
+    if team_is_full(req['team_id']):
+        return jsonify({'message': "L'équipe est pleine."}), 400
 
     db_session.add(team_user)
     db_session.commit()
-    return jsonify({'message': 'Équipe Crée'}), 200
+    return jsonify({'message': 'Équipe Créée.'}), 200
 
 
 @app.route('/api/team_users/<id>', methods=['DELETE'])
@@ -158,16 +172,16 @@ def delete_team_user(id):
     team_user = TeamUser.query.filter(TeamUser.id == id).first()
     team = Team.query.filter(Team.id == team_user.team_id).first()
     if not team_user:
-        return jsonify({'message': 'Aucune Utilisateur Trouvé'}), 500
+        return jsonify({'message': 'Aucun Utilisateur Trouvé'}), 404
 
     if team.captain_id != user_id and user_id != team_user.user_id:
         return jsonify({'message': "Vous n'êtes pas le capitaine " +
-                        "de cette équipe"}), 401
+                        "de cette équipe."}), 401
 
     db_session.delete(team_user)
     db_session.commit()
     return jsonify({'message': "cette personne à été " +
-                    "exclus de l'équipe"}), 200
+                    "exclus de l'équipe."}), 200
 
 
 @app.route('/api/teams', methods=['GET'])
@@ -195,15 +209,15 @@ def add_team():
             user_in_team(team.game, user_id) or \
             not user_has_paid_ticket(user_id):
         return jsonify({'message': "Vous avez déja une équipe pour ce jeu " +
-                        "ou le nom d'équipe est deja utilisé"}), 400
+                        "ou le nom d'équipe est deja utilisé."}), 400
 
     db_session.add(team)
     db_session.commit()
-    return jsonify({'message': 'Équipe Créé'}), 200
+    return jsonify({'message': 'Équipe Créée'}), 200
 
 
 @app.route('/api/teams/<id>', methods=['PUT'])
-def change_Team_name(id):
+def change_team_name(id):
     if 'user_id' not in session:
         return login_in_please()
     user_id = session['user_id']
@@ -212,12 +226,16 @@ def change_Team_name(id):
 
     team = Team.query.filter(Team.id == id).first()
     if not team:
-        return jsonify({'message': 'no team found'}), 500
+        return jsonify({'message': "L'équipe n'a pas été trouvée."}), 404
 
     if team.captain_id != user_id:
         return jsonify({'message':
-                        "Vous n'êtes pas le capitaine de cette équipe"}), 401
+                        "Vous n'êtes pas le capitaine de cette équipe."}), 401
     else:
+        if team_exists(team.game, req['team_name']):
+            return jsonify({'message':
+                            "Le nom d'équipe existe déjà."}), 409
+
         team.name = req['team_name']
         db_session.commit()
 
@@ -231,18 +249,18 @@ def delete_team(id):
     team = Team.query.filter(Team.id == id).first()
     members = TeamUser.query.filter(TeamUser.team_id == id)
     if not team:
-        return jsonify({'message': 'no team found'}), 500
+        return jsonify({'message': "L'équipe n'a pas été trouvée."}), 404
 
     if team.captain_id != user_id:
         return jsonify({'message':
-                        "Vous n'êtes pas le capitaine de cette équipe"}), 401
+                        "Vous n'êtes pas le capitaine de cette équipe."}), 401
     else:
         if members:
             for member in members:
                 db_session.delete(member)
         db_session.delete(team)
         db_session.commit()
-        return jsonify({'message': 'Équipe Supprimé!'}), 200
+        return jsonify({'message': "L'équipe a bien été supprimée."}), 200
 
 
 @app.route('/api/servers', methods=['GET'])
@@ -886,7 +904,7 @@ def login_in_please(message='Vous devez vous connecter.'):
 
 
 def setup(env):
-    global app, paypal_api
+    global app, paypal_api, tournaments
 
     pub_conf_path = 'config/%s_config.py' % env
     private_conf_path = 'config/secret.%s_config.py' % env
@@ -920,6 +938,8 @@ def setup(env):
         # TODO check cancel url
         cancel_url='{}/pay/cancel'.format(app.config['WEB_ROOT']))
 
+    with open('config/tournaments.json') as data_file:
+        tournaments = json.load(data_file)
     if 'STAGING' in app.config:
         app.config['CURRENT_COMMIT'] = '!!Staging is broken!!'
         set_app_commit()
